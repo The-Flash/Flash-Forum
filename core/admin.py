d4e29efc9038c01af6f700c2f2738900dd4b6c1b
Augustine Smith <absaugustineflash@gmail.com>
2021-06-10 17:48:33 +0000

    Modified Schema

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+admin.site.register(models.User)
+admin.site.register(models.Tag)
+admin.site.register(models.Topic)
+admin.site.register(models.Post)
+admin.site.register(models.FileAttachment)
--- core/models.py
+++ core/models.py
@@ -11,9 +11,21 @@ class User(AbstractUser):
     profile_photo = models.ImageField(upload_to=profile_photo_upload_to, default="profile_photos/default.png")
 
 
-class Category(models.Model):
+class Tag(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Topic(models.Model):
+    title = models.CharField(max_length=200)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) # User who posted this topic
+    tags = models.ManyToManyField("Tag") # Tags associated with a question/topic
+    description = models.TextField(blank=True) # Description given to this topic
+    created_at = models.DateTimeField(default=timezone.now)
+    attachments = models.ManyToManyField("FileAttachment", blank=True) # File attachments
 
     def __str__(self):
         return self.title
@@ -22,10 +34,9 @@ class Category(models.Model):
 class Post(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=500)
-    category = models.ForeignKey("Category", on_delete=models.CASCADE)
     attachments = models.ManyToManyField("FileAttachment", blank=True)
     content = models.TextField()
-    parent = models.OneToOneField("self", blank=True, null=True, related_name="response")
+    parent = models.ForeignKey("self", blank=True, null=True, related_name="response", on_delete=models.CASCADE)
     created_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
